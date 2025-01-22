2b0c2ed0d385bdec26a69814c3f897d291e64209
EvanLaurin <laurine@uoguelph.ca>
2025-01-21 21:32:50 -0500

    Created setup and gameloop

--- GameApp.c
+++ GameApp.c
@@ -1,17 +1,10 @@
 #include "GameApp.h"
 
+server_t server;
 
-int __cdecl main(int argc, char **argv) 
-{
-
-
-
-  WSADATA wsaData;
-  SOCKET ConnectSocket = INVALID_SOCKET;
-  struct addrinfo *result = NULL, hints;
-  enum Request request;
-  enum Response response;
-  int data, iResult;
+int connectServer(int argc, char **argv){
+  server.ConnectSocket = INVALID_SOCKET;
+  server.result = NULL;
 
   // Validate the parameters
   if (argc != 2) {
@@ -20,45 +13,124 @@ int __cdecl main(int argc, char **argv)
   }
 
   // Initialize Winsock
-  if ((iResult = WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
-    printf("WSAStartup failed with error: %d\n", iResult);
+  if ((server.iResult = WSAStartup(MAKEWORD(2, 2), &server.wsaData)) != 0) {
+    printf("WSAStartup failed with error: %d\n", server.iResult);
     return 1;
   }
 
-  ZeroMemory(&hints, sizeof(hints));
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_protocol = IPPROTO_TCP;
+  ZeroMemory(&server.hints, sizeof(server.hints));
+  server.hints.ai_family = AF_UNSPEC;
+  server.hints.ai_socktype = SOCK_STREAM;
+  server.hints.ai_protocol = IPPROTO_TCP;
 
   // Resolve the server address and port
-  if ((iResult = getaddrinfo(argv[1], DEFAULT_PORT, &hints, &result)) != 0) {
-    printf("getaddrinfo failed with error: %d\n", iResult);
+  if ((server.iResult = getaddrinfo(argv[1], DEFAULT_PORT, &server.hints, &server.result)) != 0) {
+    printf("getaddrinfo failed with error: %d\n", server.iResult);
     goto cleanup_wsa;
   }
 
     // Create a socket for connecting to the server
-  ConnectSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-  if (ConnectSocket == INVALID_SOCKET) {
+  server.ConnectSocket = socket(server.result->ai_family, server.result->ai_socktype, server.result->ai_protocol);
+  if (server.ConnectSocket == INVALID_SOCKET) {
     printf("Socket creation failed with error: %d\n", WSAGetLastError());
     goto cleanup_addrinfo;
   }
 
   // Connect to server
-  iResult = connect(ConnectSocket, result->ai_addr, (int)result->ai_addrlen);
-  if (iResult == SOCKET_ERROR) {
+  server.iResult = connect(server.ConnectSocket, server.result->ai_addr, (int)server.result->ai_addrlen);
+  if (server.iResult == SOCKET_ERROR) {
     printf("Unable to connect to server.\n");
-    closesocket(ConnectSocket);
-    ConnectSocket = INVALID_SOCKET;
+    closesocket(server.ConnectSocket);
+    server.ConnectSocket = INVALID_SOCKET;
     goto cleanup_addrinfo;
   }
 
-  freeaddrinfo(result);
+  freeaddrinfo(server.result);
 
-  if (ConnectSocket == INVALID_SOCKET) {
+  if (server.ConnectSocket == INVALID_SOCKET) {
     printf("Failed to connect to server.\n");
     goto cleanup_wsa;
   }
 
+  
+  // Send test packet
+  server.iResult = send(server.ConnectSocket, (char *)&request, sizeof(request), 0);
+  if (iResult == SOCKET_ERROR) {
+    printf("Send failed with error: %d\n", WSAGetLastError());
+    goto cleanup_socket;
+  }
+
+
+  // Receive 
+  iResult = recv(ConnectSocket, (char *)&response, sizeof(response), 0);
+  if (iResult <= 0) {
+    printf("Connection closed by server or error occurred.\n");
+    goto cleanup_socket;
+  }
+  */
+
+cleanup_socket:
+  closesocket(server.ConnectSocket);
+cleanup_addrinfo:
+  freeaddrinfo(server.result);
+cleanup_wsa:
+  WSACleanup();
+  return 0;
+}
+
+int __cdecl main(int argc, char **argv) 
+{
+
+  server.ConnectSocket = INVALID_SOCKET;
+  server.result = NULL;
+
+  // Validate the parameters
+  if (argc != 2) {
+    printf("Usage: %s <server-name>\n", argv[0]);
+    return 1;
+  }
+
+  // Initialize Winsock
+  if ((server.iResult = WSAStartup(MAKEWORD(2, 2), &server.wsaData)) != 0) {
+    printf("WSAStartup failed with error: %d\n", server.iResult);
+    return 1;
+  }
+
+  ZeroMemory(&server.hints, sizeof(server.hints));
+  server.hints.ai_family = AF_UNSPEC;
+  server.hints.ai_socktype = SOCK_STREAM;
+  server.hints.ai_protocol = IPPROTO_TCP;
+
+  // Resolve the server address and port
+  if ((server.iResult = getaddrinfo(argv[1], DEFAULT_PORT, &server.hints, &server.result)) != 0) {
+    printf("getaddrinfo failed with error: %d\n", server.iResult);
+    goto cleanup_wsa;
+  }
+
+    // Create a socket for connecting to the server
+  server.ConnectSocket = socket(server.result->ai_family, server.result->ai_socktype, server.result->ai_protocol);
+  if (server.ConnectSocket == INVALID_SOCKET) {
+    printf("Socket creation failed with error: %d\n", WSAGetLastError());
+    goto cleanup_addrinfo;
+  }
+
+  // Connect to server
+  server.iResult = connect(server.ConnectSocket, server.result->ai_addr, (int)server.result->ai_addrlen);
+  if (server.iResult == SOCKET_ERROR) {
+    printf("Unable to connect to server.\n");
+    closesocket(server.ConnectSocket);
+    server.ConnectSocket = INVALID_SOCKET;
+    goto cleanup_addrinfo;
+  }
+
+  freeaddrinfo(server.result);
+
+  if (server.ConnectSocket == INVALID_SOCKET) {
+    printf("Failed to connect to server.\n");
+    goto cleanup_wsa;
+  }
+
+  /*
   // Send 
   iResult = send(ConnectSocket, (char *)&request, sizeof(request), 0);
   if (iResult == SOCKET_ERROR) {
@@ -73,13 +145,17 @@ int __cdecl main(int argc, char **argv)
     printf("Connection closed by server or error occurred.\n");
     goto cleanup_socket;
   }
-
+  */
 
 cleanup_socket:
-  closesocket(ConnectSocket);
+  closesocket(server.ConnectSocket);
 cleanup_addrinfo:
-  freeaddrinfo(result);
+  freeaddrinfo(server.result);
 cleanup_wsa:
   WSACleanup();
   return 0;
+}
+
+void gameLoop() {
+
 }
--- GameApp.h
+++ GameApp.h
@@ -11,14 +11,26 @@
 #define DEFAULT_PORT "27015"
 #define DEFAULT_BUFLEN 512
 
+typedef struct Server {
+  WSADATA wsaData;
+  SOCKET ConnectSocket;
+  struct addrinfo *result, hints;
+  int iResult;
+} server_t;
+
+typedef struct SendPacket {
+  enum Request request;
+  int data;
+} send_packet_t;
+
 enum Request {
-  
+  //TODO: Add request types
+  echo
 };
 
 enum Response {
- 
+  //TODO: Add response types
+  echo
 };
-
-void prompt();
 
 #endif
